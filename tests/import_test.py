71e7675e2865882ee854deacc666aa46a1b59eda
Joseph C. Hardin <josephhardinee@gmail.com>
2018-05-15 16:47:19 -0700

    Added more import tests

--- tests/import_test.py
+++ tests/import_test.py
@@ -2,3 +2,7 @@ import pytest
 
 def test_import():
     import csu_radartools
+def test_import_fhc():
+    from csu_radartools import (csu_fhc, csu_liquid_ice_mass, csu_blended_rain,
+                            csu_dsd, csu_kdp, csu_misc, fundamentals)
+
